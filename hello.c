8c16d9fcfa59fd313873ed030896227c6410d492
lakshma93 <lakshmareddy93@gmail.com>
2016-03-24 19:30:54 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,7 +2,9 @@
 void main()
 {
   printf("hello this is payrole managment sample");
-  printf("this is second update");
+  printf("this is first update to the file");
+  printf("this is second updateto the file");
+  
   
   
   }
